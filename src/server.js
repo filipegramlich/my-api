33f07e25e33eaef945deb48b0e46d02a036d2b30
filipe <filipe.ogramlich@gmail.com>
2022-10-30 17:24:38 -0300

    query-params

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
+const { query } = require("express");
 const express = require("express");
 
 const app = express();
 
-app.get("/",(request, response)=>{
-    response.send("Hello, world!");
-});
+app.get("/message/:id/:user",(request, response)=>{
+    
+        const {id, user} = request.params;
+        response.send
+        (`
+            Id da mensagem ${id}.
+            Usuário ${user}.
+        `);
+    }
+);
+app.get("/users",(request, response)=>{
+    const {page, limit} = request.query;
+    response.send(`Página: ${page}. Mostrar:${limit}`)
+        
+    }
+);
 
 const PORT = 3333;
 
